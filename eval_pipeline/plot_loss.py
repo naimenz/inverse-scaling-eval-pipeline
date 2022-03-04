f0e353786d22f4cfafa854c8f485b4f1a385ca05
Ian McKenzie <ianmck98@gmail.com>
2022-03-04 12:38:56 -0800

    add plotting code (seems to work)

--- eval_pipeline/plot_loss.py
+++ eval_pipeline/plot_loss.py
@@ -7,26 +7,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+size_dict = {
+    "gpt2": 124_000_000,
+    "gpt2-medium": 355_000_000,
+    "gpt2-large": 774_000_000,
+    "gpt2-xl": 1_500_000_000,
+    # GPT-3 sizes are based on https://blog.eleuther.ai/gpt3-model-sizes/
+    "ada": 350_000_000,
+    "babbage": 1_300_000_000,
+    "curie": 6_700_000_000,
+    "davinci": 175_000_000_000,
+    # gpt neo sizes from their names
+    "gpt-neo-125M": 125_000_000,
+    "gpt-neo-1.3B": 1_300_000_000,
+    "gpt-neo-2.7B": 2_700_000_000,
+}
+
 def main():
     args = parse_args(sys.argv[1:])
-    exp_dir = Path(args.exp_dir)
-    loss_csvs = [f for f in exp_dir.glob("*.csv") if f != "data.csv"]
+    project_dir = Path(__file__).resolve().parent.parent
+    base_results_dir = Path(project_dir, "results")
+    exp_dir = Path(base_results_dir, args.exp_dir)
+    loss_csvs = [f for f in exp_dir.glob("*.csv") if f.name != "data.csv"]
 
     dfs = {csv_file.stem: pd.read_csv(csv_file, index_col=0) for csv_file in loss_csvs}
-    df = pd.DataFrame()
-        
-    averages = {col: np.mean(loss_df[col]) for col in loss_df.columns}
-    standard_errors = {col: np.std(loss_df[col]) / np.sqrt(len(model_df)) for col in loss_df.columns}
-    plot_loss(averages, standard_errors)
+
+    for name, df in dfs.items():
+        print(name)
+        print(df.head())
+
+    averages = {model_name: np.mean(df["loss"]) for model_name, df in dfs.items()}
+    standard_errors = {
+        model_name: np.std(df["loss"]) / np.sqrt(len(df["loss"]))
+        for model_name, df in dfs.items()
+    }
+    plot_loss(averages, standard_errors, exp_dir)
 
 
-
-def plot_loss(loss_dict: dict[str, float], standard_errors: dict[str, float]) -> None:
+def plot_loss(loss_dict: dict[str, float], standard_errors: dict[str, float], exp_dir: Path) -> None:
     fig = plt.figure(figsize=(20, 10))
-    errorbar_data = [(size_dict[size], loss, standard_errors[size]) for size, loss in loss_dict.items()]
+    errorbar_data = [
+        (size_dict[size], loss, standard_errors[size])
+        for size, loss in loss_dict.items()
+    ]
     xs, ys, yerrs = zip(*sorted(errorbar_data, key=lambda pair: pair[0]))
     plt.errorbar(xs, ys, yerrs)
-    
+
     labels, ticks = zip(
         *[
             (name, n_params)
@@ -39,22 +65,21 @@ def plot_loss(loss_dict: dict[str, float], standard_errors: dict[str, float]) ->
     plt.yscale("log")
     plt.xticks(ticks, labels, rotation=90)
     plt.legend()
-    plt.savefig(args.write_path)
+    plt.savefig(Path(exp_dir, "loss_plot.png"))
     plt.show()
 
+
 def parse_args(args) -> argparse.Namespace:
-    parser = argparse.ArgumentParser(
-        description="Plot models in an experiment"
-    )
+    parser = argparse.ArgumentParser(description="Plot models in an experiment")
     parser.add_argument(
         "--exp-dir",
         type=str,
         help="The name of the experiment to plot from",
         required=True,
     )
-    # args = parser.parse_args()
     args = parser.parse_args(args)
     return args
+
 
 if __name__ == "__main__":
     main()
