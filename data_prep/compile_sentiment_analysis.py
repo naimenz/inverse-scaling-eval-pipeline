561e706b7fef6fda1fa48fdccc10990be3711da5
Ian McKenzie <ianmck98@gmail.com>
2022-04-04 13:11:51 -0700

    add sentiment analysis 0shot and 1shot

--- data_prep/compile_sentiment_analysis.py
+++ data_prep/compile_sentiment_analysis.py
@@ -17,30 +17,44 @@ n_affective_pairs = 5
 n_activities = 5
 n_templates = 5
 
-filled_templates = []
-possible_answers_list = []
-answer_ix_list = []
+filled_zeroshot_templates = []
+filled_oneshot_templates = []
+classes_list = []
+answer_index_list = []
 
-sentence_template = "{name} {affective_word} {activity}"
+oneshot_template = """
+Q: Is the following sentence positive or negative? I really did not enjoy the film.
+A: The sentiment is negative.
+
+{prompt}
+""".strip()
+
+sentence_template = "{name} {affective_word} {activity}."
 for name in names["name"][:n_names]:
     for _, (positive, negative) in list(affective_words.iterrows())[:n_affective_pairs]:
         for activity in activities["activity"][:n_activities]:
             positive_sentence = sentence_template.format(name=name, affective_word=positive, activity=activity)
             negative_sentence = sentence_template.format(name=name, affective_word=negative, activity=activity)
             for _, (template, possible_answers) in list(templates_df.iterrows())[:n_templates]:
-                positive_filled_template = template.format(name=name, sentence=positive_sentence)
-                negative_filled_template = template.format(name=name, sentence=negative_sentence)
-                filled_templates.append(positive_filled_template)
-                possible_answers_list.append(possible_answers)
-                answer_ix_list.append(0)
-                filled_templates.append(negative_filled_template)
-                possible_answers_list.append(possible_answers)
-                answer_ix_list.append(1)
-                print(f"===\n{positive_filled_template}\n===")
-filled_template_df = pd.DataFrame({"filled_template": filled_templates, "possible_answers": possible_answers_list, "answer_ix": answer_ix_list})
-# reduce to just 1000 for consistency
-filled_template_df = filled_template_df[:1000]
-print(filled_template_df.head())
-print(filled_template_df.info())
-filled_template_df.to_csv(Path(processed_data_path, "sentiment_analysis.csv"))
+                positive_zeroshot_prompt = template.format(name=name, sentence=positive_sentence)
+                positive_oneshot_prompt = oneshot_template.format(prompt=positive_zeroshot_prompt)
+                filled_zeroshot_templates.append(positive_zeroshot_prompt)
+                filled_oneshot_templates.append(positive_oneshot_prompt)
+                classes_list.append(possible_answers)
+                answer_index_list.append(0)
+
+                negative_zeroshot_prompt = template.format(name=name, sentence=negative_sentence)
+                negative_oneshot_prompt = oneshot_template.format(prompt=negative_zeroshot_prompt)
+                filled_zeroshot_templates.append(negative_zeroshot_prompt)
+                filled_oneshot_templates.append(negative_oneshot_prompt)
+                classes_list.append(possible_answers)
+                answer_index_list.append(1)
+                print(f"===\n{positive_zeroshot_prompt}\n===")
+                print(f"===\n{positive_oneshot_prompt}\n===")
+zeroshot_df = pd.DataFrame({"prompt": filled_zeroshot_templates, "classes": classes_list, "answer_index": answer_index_list})
+oneshot_df = pd.DataFrame({"prompt": filled_oneshot_templates, "classes": classes_list, "answer_index": answer_index_list})
+print(zeroshot_df.head())
+print(oneshot_df.head())
+zeroshot_df.to_csv(Path(processed_data_path, "sentiment-0shot.csv"))
+oneshot_df.to_csv(Path(processed_data_path, "sentiment-1shot.csv"))
 # filled_template_df[:10].to_csv(Path(raw_data_path, "filled_templates_sample.csv"))
